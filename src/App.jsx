6d78367e44070523bbd4a93695b04da76a4c7972
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-12-05 21:02:07 +0530

    added styles for mobile view added card images added background images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
+import CardBack from "./images/bg-card-back.png";
+import CardFront from "./images/bg-card-front.png";
+import MobileBackground from "./images/bg-main-mobile.png";
+import DesktopBackground from "./images/bg-main-desktop.png";
 
 import "./App.scss";
 
 function App() {
   return (
-    <div>
-      <h1>hello from other side</h1>
-    </div>
+    <main className="header">
+      <div className="card-container">
+        <img src={MobileBackground} alt="mobile-bg" className="mobile-bg" />
+        <img src={DesktopBackground} alt="desktop-bg" className="desktop-bg" />
+        <div className="card-back">
+          <img src={CardBack} alt="card-back" />
+          {/* input numbers inside grey border */}
+        </div>
+        <div className="card-front">
+          <img src={CardFront} alt="card-front" />
+          {/* input numbers insider card */}
+        </div>
+      </div>
+      <section>
+        <div className="container"></div>
+      </section>
+    </main>
   );
 }
 
